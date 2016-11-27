4dabeb703aadb43065ee7da48ce2f0bc9b8223a3
bob poole <bob.poole@brighthr.com>
2016-11-27 12:56:11 +0000

    Werewolf login screen (not attached)

---
+++ app/src/main/java/com/example/bobpoole/werewolfclient/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.bobpoole.werewolfclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
